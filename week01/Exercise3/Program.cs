e997675b62b3100a8599bf551d7b0ffc3d15db78
Willlimaneto <awilllima@byupathway.edu>
2026-09-05 04:35:45 +0100

    Completed week 01 exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string answer2 = " ";
+        do
+        {
+            Console.WriteLine("What is the magic number? Try to guess it (__)");
+        //string answer = Console.ReadLine();
+        //int randomNum = int.Parse(answer);
+        Random random = new Random();
+        int randomNum = random.Next(1,100);
+        
+        int guessedNum = 0;
+        int guesses = 0;
+        while (guessedNum != randomNum)
+        {
+        Console.Write("What is your guess? ");
+        string answer1 = Console.ReadLine();
+        guessedNum = int.Parse(answer1);
+        guesses++;
+
+        if (guessedNum > randomNum)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guessedNum < randomNum)
+        {
+            Console.WriteLine("Higher");
+        }
+        else 
+        {
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You guessed it at your {guesses} guess.");
+            Console.WriteLine("");
+            Console.Write("Do you want to continue? ");
+            answer2 = Console.ReadLine();
+        }
+        }
+        }
+        while (answer2 == "yes");
+        
+        
     }
 }
