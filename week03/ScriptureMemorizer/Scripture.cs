166375746a19051a01988db16235aa793a469cf1
Willlimaneto <awilllima@byupathway.edu>
2026-09-19 02:50:34 +0100

    Completed Scripture Memorizer activity

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,61 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        _random = new Random();
+
+        string[] words = text.Split(' ');
+
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        int hidden = 0;
+
+        while (hidden < numberToHide && !IsCompletelyHidden())
+        {
+            int randomIndex = _random.Next(_words.Count);
+
+            if (!_words[randomIndex].IsHidden())
+            {
+                _words[randomIndex].Hide();
+                hidden++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + "\n";
+
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+
+        return displayText.Trim();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
